2a4884adf53ca4c7bae4bc62fa08825f03db935c
MaryFilipovichPersonal <maria0filipovich@gmail.com>
2021-05-30 20:20:31 +0300

    feat: done HappyArray task feat: done BillCounter task feat: done StringParser task

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -2,8 +2,22 @@ package subtask1
 
 class HappyArray {
 
-    // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        val list = sadArray.toMutableList()
+        var index = 0
+        while (index < list.size) {
+            if (index == 0 ||
+                index == list.size - 1 ||
+                (index in 1 until list.size - 1 && list[index - 1] + list[index + 1] > list[index])
+            ) {
+                index++
+                continue
+            } else {
+                list.removeAt(index)
+                index--
+            }
+        }
+        return list.toIntArray()
     }
+
 }
--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -2,8 +2,71 @@ package subtask3
 
 class StringParser {
 
-    // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val substrings: ArrayList<String> = ArrayList()
+        for ((index, value) in inputString.withIndex()) {
+            var substring: String? = null
+            when (value) {
+                '<' -> substring = parseSubstring(inputString, '<', '>', index)
+                '[' ->
+                    substring = parseSubstring(inputString, '[', ']', index)
+                '(' -> substring = parseSubstring(inputString, '(', ')', index)
+            }
+            if (substring != null) {
+                substrings.add(substring)
+            }
+
+        }
+        return substrings.toTypedArray()
+//        println("Subs: ${valid(inputString)}")
+    }
+
+    private fun parseSubstring(inputString: String, open: Char, close: Char, startIndex: Int): String? {
+        var level = 0
+        for (index in startIndex..inputString.lastIndex) {
+            when (inputString[index]) {
+                open -> level++
+                close -> if (level > 1) level-- else return inputString.substring(startIndex + 1, index)
+            }
+        }
+        return null
+    }
+
+    fun valid(inputStr: String): ArrayList<String> {
+        val s = mutableListOf<Int>()
+        val chars = inputStr.toCharArray()
+        val subs = ArrayList<String>()
+        for (i in chars.indices) {
+            val char = chars[i]
+            if (char == '(' || char == '<' || char == '[') {
+                s.add(i)
+            } else {
+                when (char) {
+                    ')' -> {
+                        val top = s.findLast { chars[it] == '('}
+                        top?.let {
+                            subs.add(inputStr.substring(it+1, i))
+                            s.remove(top)
+                        }
+                    }
+                    '>' -> {
+                        val top = s.findLast { el -> chars[el] == '<'}
+                        top?.let {
+                            subs.add(inputStr.substring(it+1, i))
+                            s.remove(top)
+                        }
+                    }
+                    ']' -> {
+                        val top = s.findLast { el -> chars[el] == '['}
+                        top?.let {
+                            subs.add(inputStr.substring(it+1, i))
+                            s.remove(top)
+                        }
+                    }
+                    else -> {}
+                }
+            }
+        }
+        return subs
     }
 }
